5527109ca0856dafcd34028ea0c2321c58512431
ju-zp <jhuntington@tutanota.com>
2018-11-19 12:15:45 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Write your solution here!
+
+const drivers = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendDriver(name){
+    drivers.push(name);
+}
+
+function destructivelyPrependDriver(name){
+    drivers.unshift(name);
+}
+
+function destructivelyRemoveLastDriver(){
+    drivers.pop();
+}
+
+function destructivelyRemoveFirstDriver(){
+    drivers.shift();
+}
+
+function appendDriver(name){
+    array = [...drivers.slice(), name]
+    return array;
+}
+
+function prependDriver(name){
+    array = [name, ...drivers.slice()];
+    return array;
+}
+
+function removeLastDriver(){
+    array = [...drivers.slice(0, drivers.length - 1)];
+    return array;
+}
+
+function removeFirstDriver(){
+    array = [...drivers.slice(1)];
+    return array;
+}
+
+
+
+
